3dcbf33a0de55981815485d0afeb3a08ceef44a0
STR <saturasstotenkopf@gmail.com>
2019-12-30 19:27:27 +0700

    Tugas 27 JS
    Mengumpulkan tugas 27 JS

---
+++ tugas27.js
@@ -0,0 +1,9 @@
+function tugas() {
+  var data = ["No","rumah saya",30, "A"];
+  var data2 = ["Jalan pangeran Tirtayasa No", 70];
+  var gabung = data.join(" ");
+  var gabung2 = data2.join(" ");
+  console.log(gabung);
+  console.log(gabung2);
+}
+tugas();
